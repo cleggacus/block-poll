d8f8dd93fa9c7bd0b366187f40f3ccd08a54b303
Liam Clegg <cleggacus@gmail.com>
2020-09-26 20:27:10 +0100

    connection

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: {
-    stunUrl: 'stun:81.110.60.116:3478',
-    turnUrl: 'turn:81.110.60.116:3478',
-    turnUsername: 'cleggacus',
-    turnCredential: '8bz82chQwXjPvpuTyaZUnktcUAWr8FFavuthZm4y',
-  },
-}
